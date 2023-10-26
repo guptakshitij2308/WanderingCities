641700d5612673d79bc3bfccc1fba50f7bf37087
Kshitij Gupta <guptakshitij2308@gmail.com>
2023-10-26 22:40:44 +0530

    feat: testing sublime merge
    testing sublime merge for project.

---
+++ wandering-cities/src/pages/AppLayout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import AppNav from "../components/AppNav";
+
+export default function AppLayout() {
+  return (
+    <div>
+      <AppNav />
+      AppLayout
+    </div>
+  );
+}
--- wandering-cities/src/pages/Homepage.jsx
+++ wandering-cities/src/pages/Homepage.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import PageNav from "../components/PageNav";
+import AppLayout from "./AppLayout";
+import AppNav from "../components/AppNav";
 
 export default function Homepage() {
   return (
     <div>
       <PageNav />
+      <AppNav />
       <h1>Wandering Cities</h1>
+
+      <Link to="/app" element={<AppLayout />}>
+        Move to app layout
+      </Link>
     </div>
   );
 }
